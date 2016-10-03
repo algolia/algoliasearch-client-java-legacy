2e554904e8614e080ee1d7090d979bd749ebf96b
Paul-Louis NECH <pln@algolia.com>
2016-10-03 16:04:26 +0200

    release.sh: also handle gradle in [INFO] mode

--- release.sh
+++ release.sh
@@ -5,7 +5,13 @@ mvn -Darguments="-DskipTests" release:perform
 
 sleep 5
 
-REP_ID=`mvn nexus-staging:rc-list | grep comalgolia | cut -d" " -f1`
+MVN_REP=`mvn nexus-staging:rc-list | grep comalgolia`
+if [[ $MVN_REP == *"[INFO]"* ]]
+then
+    REP_ID=`echo $MVN_REP | cut -d" " -f2`
+else
+    REP_ID=`echo $MVN_REP | cut -d" " -f1`
+fi
 
 if [ -z "$REP_ID" ]; then
 	echo "Can not find a REP_ID"
@@ -16,7 +22,7 @@ echo "----------------------"
 echo "REP_ID found: $REP_ID"
 echo "----------------------"
 
-sleep 5
+sleep 15 #sleep longer
 
 mvn nexus-staging:close -DstagingRepositoryId="$REP_ID"
 mvn nexus-staging:release -DstagingRepositoryId="$REP_ID"
